68dbd0c3b4857d137e769f74775f0d5d7517c4dd
JasonLee <jason@xtremeApp.com.tw>
2016-03-16 15:53:15 +0800

    first

---
+++ testProject/testProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testProject
+//
+//  Created by RD on 3/16/16.
+//  Copyright © 2016 XtremeApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testProject/testProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testProject
+//
+//  Created by RD on 3/16/16.
+//  Copyright © 2016 XtremeApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
